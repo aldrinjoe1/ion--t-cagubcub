4437d2a5e6d9e7e1cce2f143e780eb3c3ea1c5b3
aldrinjoe1 <20211254@nbsc.edu.ph>
2024-03-19 08:29:55 +0800

    added code in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {
+  //Initial Components
+  IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+} from '@ionic/react';
 
-const Tab2: React.FC = () => {
+// CSS
+//import './profile.css';
+
+const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>kapoy na</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Home</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+//
+export default Home;
