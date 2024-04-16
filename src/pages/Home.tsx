9bc81fa3aea1b1ae3ba85affb4ccade7d07d76c4
aldrinjoe1 <20211254@nbsc.edu.ph>
2024-04-16 08:53:12 +0800

    added picture in

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
         <IonToolbar>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
-        <img alt="Profile" src=" "></img>
+        <img alt="Profile" src=" https://imgs.search.brave.com/y5n__-Hp2gsu9DwhH0XQDWbG7YccFmn4d216OXnlJso/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMud2lraWEubm9j/b29raWUubmV0L2xl/YWd1ZW9mbGVnZW5k/cy9pbWFnZXMvMS8x/Ny9UZWVtb19BbG1v/c3RfSG9tZS5qcGcv/cmV2aXNpb24vbGF0/ZXN0P2NiPTIwMjIw/ODE1MTI1NDM3"></img>
         <IonCardHeader>
         <IonCardTitle>Welcome to my Application</IonCardTitle>
         <IonCardSubtitle>enjoyyyyyyy</IonCardSubtitle>
