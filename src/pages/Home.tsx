97ea305d3939e169bef3d28e443f5051542bbdc6
aldrinjoe1 <20211254@nbsc.edu.ph>
2024-04-19 08:24:06 +0800

    added changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import {
-  //Initial Components
-  IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard,
-  IonCardContent, IonCardHeader,IonCardSubtitle, IonCardTitle
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle
 } from '@ionic/react';
-
-// CSS
-//import './profile.css';
 
 const Home: React.FC = () => {
   return (
@@ -14,25 +19,20 @@ const Home: React.FC = () => {
         <IonToolbar>
           <IonTitle>Home</IonTitle>
         </IonToolbar>
-        <img alt="Profile" src=" https://imgs.search.brave.com/y5n__-Hp2gsu9DwhH0XQDWbG7YccFmn4d216OXnlJso/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMud2lraWEubm9j/b29raWUubmV0L2xl/YWd1ZW9mbGVnZW5k/cy9pbWFnZXMvMS8x/Ny9UZWVtb19BbG1v/c3RfSG9tZS5qcGcv/cmV2aXNpb24vbGF0/ZXN0P2NiPTIwMjIw/ODE1MTI1NDM3"></img>
-        <IonCardHeader>
-        <IonCardTitle>Welcome to my Application</IonCardTitle>
-        <IonCardSubtitle>enjoyyyyyyy fellas</IonCardSubtitle>
-      </IonCardHeader>
-
-      <IonCardContent>feel free to contact me when you have concern, lovee yahhhh:0</IonCardContent>
-      
       </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonCard>
+          <img src="https://imgs.search.brave.com/y5n__-Hp2gsu9DwhH0XQDWbG7YccFmn4d216OXnlJso/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMud2lraWEubm9j/b29raWUubmV0L2xl/YWd1ZW9mbGVnZW5k/cy9pbWFnZXMvMS8x/Ny9UZWVtb19BbG1v/c3RfSG9tZS5qcGcv/cmV2aXNpb24vbGF0/ZXN0P2NiPTIwMjIw/ODE1MTI1NDM3" alt="Profile" />
+          <IonCardHeader>
+            <IonCardTitle>Welcome to my Application</IonCardTitle>
+            <IonCardSubtitle>enjoyyyyyyy fellas</IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>Feel free to contact me when you have concerns. Love yahhhh:)</IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
 };
 
-//
 export default Home;
