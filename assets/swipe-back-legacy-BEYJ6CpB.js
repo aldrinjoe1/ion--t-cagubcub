3dada0d32a22eab47df4c981785eaba348f81ed3
adlrinjoe1 <20211254@nbsc.edu.ph>
2024-05-10 08:19:07 +0800

    Updates

--- assets/swipe-back-legacy-BEYJ6CpB.js
+++ assets/swipe-back-legacy-BEYJ6CpB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DfyBB82Z.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-B_yYuM0v.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
